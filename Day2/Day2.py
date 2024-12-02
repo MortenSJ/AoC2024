15a6edaa3c5e8199eb88cfe881181cfe048783af
Morten Sigaard Jacobsen <msigj@holstebro.dk>
2024-12-02 14:40:26 +0100

    argh!!

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -4,10 +4,23 @@ sum = 0
 
 for i in f:
     list_of_reports = i.strip().split(" ")
-    #print(list_of_reports)
-    for x in range(0,len(list_of_reports)):
-        if int(list_of_reports[x])-int(list_of_reports[int(x)+1]) >3 or int(list_of_reports[x])-int(list_of_reports[int(x)+1]) < 1:
-            break
-        else: sum = sum +1
+    add_to_safe_list = False
+    for x in range(0,len(list_of_reports)-1):
+
+        if abs((int(list_of_reports[x])-int(list_of_reports[int(x)+1])) >3 or abs(int(list_of_reports[x])-int(list_of_reports[int(x)+1])) < 1) and (all(list_of_reports[i] <= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == False or all(list_of_reports[i] >= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) ==False):
+            add_to_safe_list = False
+            print("false " + str(list_of_reports))   
+        else: 
+            add_to_safe_list = True
+
+        """For checking ascending order
+        if all(list_of_reports[i] <= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == True or all(list_of_reports[i] >= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) ==True:
+            print(f'List is decending og accending')
+        else:
+            add_to_safe_list = False"""
+        
+    if add_to_safe_list == True:
+        print("True " + str(list_of_reports))
+        sum = sum + 1
 
 print(f'The sum for the first task is: {sum}')
