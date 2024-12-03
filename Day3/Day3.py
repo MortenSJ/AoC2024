90026e90f166afc3871bd61071dcd417ff853097
MortenSJ <morten.sigjac@gmail.com>
2024-12-03 23:04:45 +0100

    Day3_1 complet

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,3 +1,5 @@
+import re
+
 #Open and read file
 f = open("Day3\\input.txt", "r")
 sum = 0
@@ -5,13 +7,49 @@ sum = 0
 muls = []
 
 for i in f: 
-    mul = str(i).split("mul(")
-    mul = str(mul).split(")")
-    print(mul)
-    muls.append(mul)
+    mul3 = re.findall("mul\(.{1,4},.{1,4}\)", i)
+    
+    for m in mul3:
+        muls.append(m)
 
-for m in muls:
-    if str(m).split(,)
+for x in muls:
+    x = str(x).replace("mul(","").replace(")","").replace("[","").replace("]","").replace("'","")
 
+    if x != '':
+        first_val = int(x.split(",")[0])
+        sec_val = int(x.split(",")[1])
+        multiply = first_val*sec_val
+
+    sum = sum + multiply
 
 print(f'The sum for the first task is: {sum}')
+
+#Task two!!
+#start_pos = muls[0]
+sum=0
+muls2 = []
+
+f = open("Day3\\input.txt", "r")
+
+for o in f: 
+    print("Finding mul...")
+    mul = re.findall("do\(\).{0,2}mul\(.{1,4},.{1,4}\)", o)
+    
+    print(f'Mul is {mul}')
+    for n in mul:
+        muls2.append(n)
+
+for c in muls2:
+    c = str(x).replace("mul(","").replace(")","").replace("[","").replace("]","").replace("'","")
+
+    if c != '':
+        first_val = int(x.split(",")[0])
+        sec_val = int(x.split(",")[1])
+        multiply = first_val*sec_val
+
+    sum = sum + multiply
+
+#sum = sum + start_pos
+print(f'The sum for the sec task is: {sum}')
+
+#1624642 too low
