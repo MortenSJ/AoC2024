cafb75700ceecfbfccee3095d592f07949e69c02
MortenSJ <morten.sigjac@gmail.com>
2024-12-03 23:47:59 +0100

    Day2 still argh

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -5,22 +5,36 @@ sum = 0
 for i in f:
     list_of_reports = i.strip().split(" ")
     add_to_safe_list = False
-    for x in range(0,len(list_of_reports)-1):
-
-        if abs((int(list_of_reports[x])-int(list_of_reports[int(x)+1])) >3 or abs(int(list_of_reports[x])-int(list_of_reports[int(x)+1])) < 1) and (all(list_of_reports[i] <= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == False or all(list_of_reports[i] >= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) ==False):
-            add_to_safe_list = False
-            #print("false " + str(list_of_reports))   
-        else: 
-            add_to_safe_list = True
-
-        """For checking ascending order
-        if all(list_of_reports[i] <= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == True or all(list_of_reports[i] >= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) ==True:
-            print(f'List is decending og accending')
-        else:
-            add_to_safe_list = False"""
+    print(f"start {str(list_of_reports)}") 
+    #Ascending check
+    if all(list_of_reports[i] <= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == True:
+        print(f'list is ascending')
+        add_to_safe_list = True
+    
+    #descending check
+    if add_to_safe_list == False and all(list_of_reports[i] >= list_of_reports[i + 1] for i in range(len(list_of_reports) - 1)) == True:
+        print(f'list is descending')
+        add_to_safe_list = True
+    if add_to_safe_list == True:
+        for x in range(0,len(list_of_reports)-1):
+            print(f'list_of_reports[x] is: {list_of_reports[x]} and list_of_reports[int(x)+1] is: {list_of_reports[int(x)+1]}')
+            
+            #Adding check
+            diff = abs((int(list_of_reports[int(x)])-int(list_of_reports[int(x)+1]))) #< 3 or abs(int(list_of_reports[int(x)])-int(list_of_reports[int(x)+1])) > 0):
+            if diff > 0 and diff < 4:
+                add_to_safe_list = True
+                print(f'Diff is: {abs((int(list_of_reports[x])-int(list_of_reports[int(x)+1])))}')
+            else: 
+                add_to_safe_list = False
+                break
         
     if add_to_safe_list == True:
         print("True " + str(list_of_reports))
         sum = sum + 1
+    else:
+        print("Fasle " + str(list_of_reports))
 
 print(f'The sum for the first task is: {sum}')
+
+#221 is too low
+#530 is too high
