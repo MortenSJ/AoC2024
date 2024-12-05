793169f94e1eb7773572d01ceeb897783459fe32
Morten Sigaard Jacobsen <msigj@holstebro.dk>
2024-12-05 21:38:36 +0100

    Day 5 added

---
+++ Day4/Day4.py
@@ -0,0 +1,13 @@
+import re
+#Open and read file
+f = open("Day4\\input.txt", "r")
+sum = 0
+
+xmas_total=[]
+
+for i in f:
+    if i == "X":
+        #håndtering af kuben
+        
+
+print(f'The sum for the first task is: {sum}')
---
+++ Day5/Day5.py
@@ -0,0 +1,6 @@
+#Open and read file
+f = open("Day5\\input.txt", "r")
+sum = 0
+
+
+print(f'The sum for the first task is: {sum}')
